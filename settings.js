538b6b39f63017cb61f587f0b4c0bbc389cfcea0
Muhammad Ircham Febriansyah <109428078+mhmdircham@users.noreply.github.com>
2023-04-10 23:14:52 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'L~Cham'// yourname
+global.MONGO_DB_URI = "mongodb+srv://ircham:ircham0506@cluster0.hngmke5.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "Ircham0506!" //isi apa aja bebas
+global.your_email = "masirchamganteng@gmail.com" //email
+global.email_password = "vlzc fwtm gvrh lkda" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
